6ce9ade9d72b165ad0ea66fa7160c6c2f4de4e01
fasoxy-it <fasoxy@gmail.com>
2021-04-12 15:04:47 +0200

    Initial Commit

---
+++ Biography-iOS/Biography_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biography_iOSApp.swift
+//  Biography-iOS
+//
+//  Created by Mattia Fasoli on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Biography_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
